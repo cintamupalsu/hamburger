330fd14e5e8afd1c43a3ab09773242d7a8e8c37f
a_maulana <cintamupalsu@hotmail.com>
2018-11-11 20:01:58 +0900

    Done with layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root 'desk_pages#home'
-  get 'desk_pages/home'
-  get 'desk_pages/help'
-  get 'desk_pages/about'
-  get 'desk_pages/contact'
+  #get 'desk_pages/home'
+  get 'help', to:'desk_pages#help'
+  get 'about', to:'desk_pages#about'
+  get 'contact', to:'desk_pages#contact'
+  get 'signup', to:'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
- 
 end
--- test/controllers/desk_pages_controller_test.rb
+++ test/controllers/desk_pages_controller_test.rb
@@ -6,31 +6,31 @@ class DeskPagesControllerTest < ActionDispatch::IntegrationTest
   end
   
   test "should get root" do
-    get root_url
+    get root_path
     assert_response :success
   end
   
-  test "should get home" do
-    get desk_pages_home_url
-    assert_response :success
-    #assert_select "title","Home | Hamburger App"
-    assert_select "title","Hamburger Here"
-  end
+  #test "should get home" do
+  #  get desk_pages_home_url
+  #  assert_response :success
+  #  #assert_select "title","Home | Hamburger App"
+  #  assert_select "title","Hamburger Here"
+  #end
 
   test "should get help" do
-    get desk_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "Help | Hamburger Here"
   end
   
   test "should get about" do
-    get desk_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About | Hamburger Here"
   end
   
   test "should get contact" do
-    get desk_pages_contact_url
+    get contact_path
     assert_response :success
     assert_select "title", "Contact | #{@base_title}"
   end
