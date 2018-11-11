fd10a2c071247713d355f71fa1a0a21e1ee519c2
a_maulana <cintamupalsu@hotmail.com>
2018-11-11 10:22:31 +0900

    Title change

--- test/controllers/desk_pages_controller_test.rb
+++ test/controllers/desk_pages_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class DeskPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
-    @base_title = "Hamburger App"
+    @base_title = "Hamburger Here"
   end
   
   test "should get root" do
@@ -13,19 +13,20 @@ class DeskPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get desk_pages_home_url
     assert_response :success
-    assert_select "title","Home | Hamburger App"
+    #assert_select "title","Home | Hamburger App"
+    assert_select "title","Hamburger Here"
   end
 
   test "should get help" do
     get desk_pages_help_url
     assert_response :success
-    assert_select "title", "Help | Hamburger App"
+    assert_select "title", "Help | Hamburger Here"
   end
   
   test "should get about" do
     get desk_pages_about_url
     assert_response :success
-    assert_select "title", "About | Hamburger App"
+    assert_select "title", "About | Hamburger Here"
   end
   
   test "should get contact" do
